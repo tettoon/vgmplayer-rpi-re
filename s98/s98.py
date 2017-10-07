28a52d6f51e03763bfb477a60e9e3128a24aa242
Tettoon <tettoon@gmail.com>
2017-10-08 02:01:45 +0900

    Add S98 support.

---
+++ s98/s98.py
@@ -0,0 +1,209 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import array
+import sys
+import time
+
+DEVICE_NONE = 0
+DEVICE_SSG = 1
+DEVICE_OPN = 2
+DEVICE_OPN2 = 3
+DEVICE_OPNA = 4
+DEVICE_OPM = 5
+DEVICE_OPLL = 6
+DEVICE_OPL = 7
+DEVICE_OPL2 = 8
+DEVICE_OPL3 = 9
+DEVICE_PSG = 15
+DEVICE_DCSG = 16
+
+class S98:
+
+    MAGIC = bytearray('S98', 'latin-1')
+
+    def __init__(self, buffer):
+        self.buffer = buffer
+        self.playing = False
+        self.stopped = True
+        self.repeat = False
+        self.loop_count = 1
+        self.reset_handlers = []
+        self.write_handlers = []
+        self.mute_handlers = []
+
+        self.__load_header()
+
+    def __load_header(self):
+
+        self.buffer.seek(0)
+
+        # ident
+        ident = bytearray(self.buffer.read(len(self.MAGIC)))
+        for (a, b) in zip(self.MAGIC, ident):
+            if a != b:
+                raise S98Error('Invalid file identification.')
+
+        # header
+        self.version = self.read_int8(self.buffer)
+
+        self.timer_info = self.read_int32(self.buffer)
+        if self.timer_info == 0:
+            self.timer_info = 10
+        self.timer_info2 = self.read_int32(self.buffer)
+        if self.timer_info2 == 0:
+            self.timer_info2 = 1000
+        self.resolution = self.timer_info2 / self.timer_info
+
+        self.comressing = self.read_int32(self.buffer)
+        self.file_offset_tag = self.read_int32(self.buffer)
+        self.file_offset_dump_data = self.read_int32(self.buffer)
+        self.file_offset_loop_point = self.read_int32(self.buffer)
+        self.device_count = self.read_int32(self.buffer)
+        self.device_info = []
+        if self.device_count == 0:
+            self.device_info.append({
+                "device_type": 4,
+                "clock": 7987200,
+                "pan": 0,
+            })
+            self.device_count = 1
+        else:
+            if self.device_count > 64:
+                raise S98Error('Device count must be less than 64.')
+            for i in range(self.device_count):
+                self.device_info.append({
+                    "device_type": self.read_int32(self.buffer),
+                    "clock": self.read_int32(self.buffer),
+                    "pan": self.read_int32(self.buffer),
+                })
+                self.read_int32(self.buffer)
+
+        # tag
+        if self.file_offset_tag != 0:
+            self.buffer.seek(self.file_offset_tag)
+            print("0x{0:X}".format(self.buffer.tell()))
+            self.tag = self.buffer.readlines()
+        else:
+            self.tag = None
+
+        # Dump data
+        self.buffer.seek(self.file_offset_dump_data)
+
+    def play(self):
+        self.__samples = 0
+
+        self.__fire_reset()
+
+        self.__origin_time = time.time() + 0.5
+
+        self.playing = True
+        self.stopped = False
+        while self.playing:
+            command = self.read_int8(self.buffer)
+            # print("Command {0:X} found.".format(command))
+
+            if command == 0xff:
+                self.__wait_samples(1)
+            elif command == 0xfe:
+                s = 0
+                n = 0
+                c = True
+                while c:
+                    p = self.read_int8(self.buffer)
+                    n |= (p & 0x7f) << s
+                    s += 7
+                    c = (p & 0x80) != 0
+                self.__wait_samples(n+2)
+            elif command == 0xfd:
+                if self.file_offset_loop_point == 0:
+                    self.playing = False
+                elif self.repeat:
+                    self.buffer.seek(self.file_offset_loop_point)
+                elif self.loop_count > 1:
+                    self.loop_count -= 1
+                    self.buffer.seek(self.file_offset_loop_point)
+                else:
+                    self.playing = False
+            else:
+                device_num = (command >> 1) & 0x7f
+                # print("{0}, {1}".format(device_num, self.device_count))
+                if device_num >= self.device_count:
+                    raise S98Error("Invalid device number: {0}".format(device_num))
+                extend = command & 1
+                address = self.read_int8(self.buffer)
+                data = self.read_int8(self.buffer)
+
+                device_info = self.device_info[device_num]
+                device_type = device_info['device_type']
+                device_name = None
+                if device_type == DEVICE_NONE:
+                    pass
+                elif device_type == DEVICE_OPN:
+                    if extend == 0:
+                        self.__fire_write('YM2203', address, data)
+                elif device_type == DEVICE_OPNA:
+                    if extend == 0:
+                        self.__fire_write('YM2608', address, data)
+                    else:
+                        self.__fire_write('YM2608', address | 0x100, data)
+                else:
+                    raise S98Error("Unsupported command: 0x{0:X}".format(command))
+
+            while self.__samples > (time.time()-self.__origin_time)*self.resolution:
+                pass
+
+        self.__fire_mute()
+        self.playing = False
+        self.stopped = True
+
+    def stop(self):
+        self.playing = False
+
+    @classmethod
+    def read_int32(self, buffer):
+        data = bytearray(buffer.read(4))
+        if len(data) == 4:
+           return data[0] | data[1] << 8 | data[2] << 16 | data[3] << 24
+        else:
+           raise S98Error("Unexpected EOF.")
+
+    @classmethod
+    def read_int16(self, buffer):
+        data = bytearray(buffer.read(2))
+        if len(data) == 2:
+            return data[0] | data[1] << 8
+        else:
+           raise S98Error("Unexpected EOF.")
+
+    @classmethod
+    def read_int8(self, buffer):
+        data = bytearray(buffer.read(1))
+        if len(data) == 1:
+           return data[0]
+        else:
+           raise S98Error("Unexpected EOF.")
+
+    @classmethod
+    def read_string(self, buffer):
+        pass
+
+    def __wait_samples(self, samples):
+        self.__samples += samples
+
+    def __fire_reset(self):
+        for h in self.reset_handlers:
+            h()
+
+    def __fire_write(self, name, address, data):
+        for h in self.write_handlers:
+            h(name, address, data)
+
+    def __fire_mute(self):
+        for h in self.mute_handlers:
+            h()
+
+
+class S98Error(Exception):
+
+    def __init__(self, message):
+        self.message = message
+
---
+++ s98player.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+
+from __future__ import division, print_function, unicode_literals
+import argparse
+import io
+import os
+import signal
+import sys
+import time
+
+from module_controller import ModuleController
+from m3u import M3U
+from s98.s98 import S98, S98Error
+
+
+class S98Player:
+
+    playlist = False
+    show_tag = False
+    cancel = False
+    repeat = False
+    loop_count = 1
+
+    def __init__(self):
+        self.mc = ModuleController()
+
+    @property
+    def modules(self):
+        return self.mc.modules
+
+    def play(self, filename):
+        if self.playlist:
+            self.__playlist(filename)
+        else:
+            self.__play(filename)
+
+    def __play(self, filename):
+        file_bytes = None
+
+        with io.FileIO(filename, 'rb') as f:
+            file_bytes = f.readall()
+
+        with io.BytesIO(file_bytes) as data:
+            self.__play_s98(data)
+
+    def __play_s98(self, f):
+        self.s98 = S98(f)
+        self.s98.reset_handlers.append(self.mc.reset)
+        self.s98.write_handlers.append(self.mc.write)
+        self.s98.mute_handlers.append(self.mc.mute)
+        self.s98.repeat = self.repeat
+        self.s98.loop_count = self.loop_count
+
+        # tag = self.s98.tag
+        if self.show_tag == True and self.s98.tag is not None:
+            print(self.s98.tag)
+
+        self.s98.play()
+
+    def __playlist(self, filename):
+        self.playlist = M3U(filename)
+        while not self.cancel:
+            for file in self.playlist.files:
+                if self.cancel:
+                    break
+                print(file)
+                self.__play(file)
+
+    def stop(self):
+        if self.s98 is not None and self.s98.playing:
+            self.s98.stop()
+            while self.s98.playing and not self.s98.stopped:
+                pass
+            del self.s98
+            self.cancel = True
+
+
+def break_handler(signal, frame):
+    print("Interrupted.")
+    player.stop()
+
+
+parser = argparse.ArgumentParser(description='Playback S98 data.')
+parser.add_argument("-t", "--tag", action="store_true", help='show tag')
+parser.add_argument("-l", "--list", action="store_true", help='load M3U playlist')
+parser.add_argument("-m", "--module", type=str, help='RE:birth module identifier')
+parser.add_argument("-r", "--repeat", type=int, help='Repeat song')
+parser.add_argument("file", type=str, help="S98 file")
+args = parser.parse_args()
+
+signal.signal(signal.SIGINT, break_handler)
+
+player = S98Player()
+player.modules[0] = args.module
+if args.tag:
+    player.show_tag = True
+if args.list:
+    player.playlist = True
+if args.repeat != None:
+    if args.repeat > 0:
+        player.loop_count = args.repeat
+    else:
+        player.repeat = True
+
+player.play(args.file)
+
